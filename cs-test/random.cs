5a33452e20becb7da917147a992626d75c228099
Michael Graff <explorer@flame.org>
2021-02-14 01:27:40 -0600

    use the slightly different random code the game uses

--- cs-test/random.cs
+++ cs-test/random.cs
@@ -1,6 +1,125 @@
 using System;
 using System.IO;
 
+using System.Runtime.InteropServices;
+
+namespace System2
+{
+	public class Random
+	{
+		public Random() : this(Environment.TickCount)
+		{
+		}
+
+		public Random(int Seed)
+		{
+			int num = 161803398 - Math.Abs(Seed);
+			this.SeedArray[55] = num;
+			int num2 = 1;
+			for (int i = 1; i < 55; i++)
+			{
+				int num3 = 21 * i % 55;
+				this.SeedArray[num3] = num2;
+				num2 = num - num2;
+				if (num2 < 0)
+				{
+					num2 += int.MaxValue;
+				}
+				num = this.SeedArray[num3];
+			}
+			for (int j = 1; j < 5; j++)
+			{
+				for (int k = 1; k < 56; k++)
+				{
+					this.SeedArray[k] -= this.SeedArray[1 + (k + 30) % 55];
+					if (this.SeedArray[k] < 0)
+					{
+						this.SeedArray[k] += int.MaxValue;
+					}
+				}
+			}
+			this.inext = 0;
+			this.inextp = 31;
+		}
+
+		protected virtual double Sample()
+		{
+			if (++this.inext >= 56)
+			{
+				this.inext = 1;
+			}
+			if (++this.inextp >= 56)
+			{
+				this.inextp = 1;
+			}
+			int num = this.SeedArray[this.inext] - this.SeedArray[this.inextp];
+			if (num < 0)
+			{
+				num += int.MaxValue;
+			}
+			this.SeedArray[this.inext] = num;
+			return (double)num * 4.656612875245797E-10;
+		}
+
+		public virtual int Next()
+		{
+			return (int)(this.Sample() * 2147483647.0);
+		}
+
+		public virtual int Next(int maxValue)
+		{
+			if (maxValue < 0)
+			{
+				throw new ArgumentOutOfRangeException("Max value is less than min value.");
+			}
+			return (int)(this.Sample() * (double)maxValue);
+		}
+
+		public virtual int Next(int minValue, int maxValue)
+		{
+			if (minValue > maxValue)
+			{
+				throw new ArgumentOutOfRangeException("Min value is greater than max value.");
+			}
+			uint num = (uint)(maxValue - minValue);
+			if (num <= 1U)
+			{
+				return minValue;
+			}
+			return (int)((ulong)((uint)(this.Sample() * num)) + (ulong)((long)minValue));
+		}
+
+		public virtual void NextBytes(byte[] buffer)
+		{
+			if (buffer == null)
+			{
+				throw new ArgumentNullException("buffer");
+			}
+			for (int i = 0; i < buffer.Length; i++)
+			{
+				buffer[i] = (byte)(this.Sample() * 256.0);
+			}
+		}
+
+		public virtual double NextDouble()
+		{
+			return this.Sample();
+		}
+
+		private const int MBIG = 2147483647;
+
+		private const int MSEED = 161803398;
+
+		private const int MZ = 0;
+
+		private int inext;
+
+		private int inextp;
+
+		private int[] SeedArray = new int[56];
+	}
+}
+
 //
 // Create a set of random values for testing.  Ideally,
 // we will test all the functions we use in the rest of the
@@ -31,7 +150,7 @@ public class RandomTest
 
     private static void generateNext(int count, string filename, int seed)
     {
-        Random r = new Random(seed);
+        System2.Random r = new System2.Random(seed);
         using (StreamWriter file = new StreamWriter(filename))
         {
             for (int i = 0 ; i < count ; i++) {
@@ -42,7 +161,7 @@ public class RandomTest
 
     private static void generateNextDouble(int count, string filename, int seed)
     {
-        Random r = new Random(seed);
+        System2.Random r = new System2.Random(seed);
         using (StreamWriter file = new StreamWriter(filename))
         {
             for (int i = 0 ; i < count ; i++) {
@@ -53,7 +172,7 @@ public class RandomTest
 
     private static void generateNextMax(int count, string filename, int seed)
     {
-        Random r = new Random(seed);
+        System2.Random r = new System2.Random(seed);
         using (StreamWriter file = new StreamWriter(filename))
         {
             for (int i = 0 ; i < count ; i++) {
@@ -64,7 +183,7 @@ public class RandomTest
 
     private static void generateNextRange(int count, string filename, int seed)
     {
-        Random r = new Random(seed);
+        System2.Random r = new System2.Random(seed);
         using (StreamWriter file = new StreamWriter(filename))
         {
             for (int i = 0 ; i < count ; i++) {
--- internal/cs/random.go
+++ internal/cs/random.go
@@ -25,26 +25,18 @@ func iabs(x int32) int32 {
 func MakePRNGSequence(seed int32) *PRNGSequence {
 	r := &PRNGSequence{
 		inext:     0,
-		inextp:    21,
+		inextp:    31,
 		seedarray: make([]int32, 56),
 	}
 
 	var ii int32
 
-	var subtraction int32
-	if seed == math.MinInt32 {
-		subtraction = math.MaxInt32
-	} else {
-		subtraction = iabs(seed)
-	}
-
-	mj := 161803398 - subtraction
+	mj := 161803398 - iabs(seed)
 	r.seedarray[55] = mj
 
 	var mk int32 = 1
-	var i int32
-	for i = 1; i < 55; i++ {
-		ii = (21 * i) % 55
+	for i := int32(1); i < 55; i++ {
+		ii = 21 * i % 55
 		r.seedarray[ii] = mk
 		mk = mj - mk
 		if mk < 0 {
@@ -53,7 +45,7 @@ func MakePRNGSequence(seed int32) *PRNGSequence {
 		mj = r.seedarray[ii]
 	}
 	for k := 1; k < 5; k++ {
-		for i = 1; i < 56; i++ {
+		for i := 1; i < 56; i++ {
 			r.seedarray[i] -= r.seedarray[1+(i+30)%55]
 			if r.seedarray[i] < 0 {
 				r.seedarray[i] += math.MaxInt32
@@ -68,10 +60,36 @@ func MakePRNGSequence(seed int32) *PRNGSequence {
 // Next returns the next value in the sequence.
 //
 func (r *PRNGSequence) Next() int32 {
+	return int32(r.internalSample() * math.MaxInt32)
+}
+
+//
+// NextDouble returns a floating point value between 0 and 1, inclusive.
+//
+func (r *PRNGSequence) NextDouble() float64 {
 	return r.internalSample()
 }
 
-func (r *PRNGSequence) internalSample() int32 {
+//
+// NextWithMax returns a random value between 0 and the max value provided,
+// inclusive.
+//
+func (r *PRNGSequence) NextWithMax(max int32) int32 {
+	return int32(r.internalSample() * float64(max))
+}
+
+//
+// NextRange returns a value between min and max, inclusive.
+//
+func (r *PRNGSequence) NextRange(min int32, max int32) int32 {
+	span := uint32(max - min)
+	if span <= 1 {
+		return min
+	}
+	return int32(uint64(uint32(r.internalSample()*float64(span))) + uint64(int64(min)))
+}
+
+func (r *PRNGSequence) internalSample() float64 {
 	var locINext = r.inext
 	var locINextp = r.inextp
 
@@ -84,9 +102,6 @@ func (r *PRNGSequence) internalSample() int32 {
 		locINextp = 1
 	}
 	retVal := r.seedarray[locINext] - r.seedarray[locINextp]
-	if retVal == math.MaxInt32 {
-		retVal--
-	}
 	if retVal < 0 {
 		retVal += math.MaxInt32
 	}
@@ -95,47 +110,5 @@ func (r *PRNGSequence) internalSample() int32 {
 	r.inext = locINext
 	r.inextp = locINextp
 
-	return retVal
-}
-
-//
-// NextDouble returns a floating point value between 0 and 1, inclusive.
-//
-func (r *PRNGSequence) NextDouble() float64 {
-	return r.sample()
-}
-
-func (r *PRNGSequence) sample() float64 {
-	return float64(r.internalSample()) * (1.0 / float64(math.MaxInt32))
-}
-
-//
-// NextWithMax returns a random value between 0 and the max value provided,
-// inclusive.
-//
-func (r *PRNGSequence) NextWithMax(max int32) int32 {
-	return int32(r.NextDouble() * float64(max))
-}
-
-//
-// NextRange returns a value between min and max, inclusive.
-//
-func (r *PRNGSequence) NextRange(min int32, max int32) int32 {
-	span := int64(max) - int64(min)
-	if span <= math.MaxInt32 {
-		return int32(r.sample()*float64(span)) + min
-	}
-	return int32(r.getSampleForLargeRange()*float64(span)) + min
-}
-
-func (r *PRNGSequence) getSampleForLargeRange() float64 {
-	result := r.internalSample()
-	negative := r.internalSample()%2 == 0
-	if negative {
-		result = -result
-	}
-	d := float64(result)
-	d += (math.MaxInt32 - 1)
-	d /= 2*math.MaxInt32 - 1
-	return d
+	return float64(retVal) * 4.656612875245797e-10
 }

6135740b7cbafc91dbc44643dd0d011785df509c
Michael Graff <explorer@flame.org>
2021-02-14 02:05:44 -0600

    more warning supression

--- cmd/parsefile/planet_type.go
+++ cmd/parsefile/planet_type.go
@@ -1,14 +1,26 @@
 package main
 
+// PlanetType indicates the type of the planet's surface.
 type PlanetType int32
 
 const (
-	PlanetTypeNone    PlanetType = 0
+	// PlanetTypeNone means it doesn't exist I suppose?
+	PlanetTypeNone PlanetType = 0
+
+	// PlanetTypeVolcano are hot and likely rocky.
 	PlanetTypeVolcano PlanetType = 1
-	PlanetTypeOcean   PlanetType = 2
-	PlanetTypeDesert  PlanetType = 3
-	PlanetTypeIce     PlanetType = 4
-	PlanetTypeGas     PlanetType = 5
+
+	// PlanetTypeOcean means you'll need lots of landfill to mine.
+	PlanetTypeOcean PlanetType = 2
+
+	// PlanetTypeDesert are dry most likely.
+	PlanetTypeDesert PlanetType = 3
+
+	// PlanetTypeIce are ice giants.
+	PlanetTypeIce PlanetType = 4
+
+	// PlanetTypeGas are gas giants.
+	PlanetTypeGas PlanetType = 5
 )
 
 func (t PlanetType) String() string {
--- cmd/parsefile/spectral_type.go
+++ cmd/parsefile/spectral_type.go
@@ -4,28 +4,28 @@ package main
 type SpectralType int32
 
 const (
-	// SpectralTypeM are type M.
+	// SpectralTypeM are red, colder stars, < 3500K, and dim.
 	SpectralTypeM SpectralType = 0
 
-	// SpectralTypeK are type K.
+	// SpectralTypeK are orange to red, 3500 - 5000K, and dimmer than our sun.
 	SpectralTypeK SpectralType = 1
 
-	// SpectralTypeG are type G.
+	// SpectralTypeG are white to yellow, 5000 - 6000K.
 	SpectralTypeG SpectralType = 2
 
-	// SpectralTypeF are type F.
+	// SpectralTypeF are blue to white, 6000 - 7500K, 6x brighter than our sun.
 	SpectralTypeF SpectralType = 3
 
-	// SpectralTypeA are type A.
+	// SpectralTypeA are blue, 7500 - 11000K, 80x brighter than our sun.
 	SpectralTypeA SpectralType = 4
 
-	// SpectralTypeB are type B.
+	// SpectralTypeB are blue, larger, and 20,000x brighter than our sun.
 	SpectralTypeB SpectralType = 5
 
-	// SpectralTypeO are type O.
+	// SpectralTypeO are blue giants, over 25000K, and 1,400,000x brighter than our sun.
 	SpectralTypeO SpectralType = 6
 
-	// SpectralTypeX are type X.
+	// SpectralTypeX are special, in that they are neutron stars or black holes.
 	SpectralTypeX SpectralType = 7
 )
 
